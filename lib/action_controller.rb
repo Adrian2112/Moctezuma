a3b171f5543ae7f3ef6e44d9b12dc943e4c64b16
Adrian Gzz <bernardogzzf@gmail.com>
2010-11-19 00:23:45 -0600

    se agrego la funcion para desplegar el layout de las vistas

--- lib/action_controller.rb
+++ lib/action_controller.rb
@@ -9,22 +9,34 @@ class ActionController
   end
   
   def render
+    view = get_view_file
+    return layout { view }
+  end
+  
+  def layout
     if self.html.nil?
       begin
-        vista = File.new("./app/views/#{self.params[:controller]}/#{self.params[:action]}.html.erb").read
-        self.html = ERB.new(vista).result(self.get_binding)
+        vista = File.new("./app/views/layout/application_layout.html.erb").read
+        self.html = ERB.new(vista).result(binding)
       rescue Errno::ENOENT
         vista = File.new("./404.html").read
         self.html = vista
       end
     end
-    
     return self.html
   end
   
-  
-  def get_binding
-    return binding
+  def get_view_file
+    view_html = ""
+    begin
+      vista = File.new("./app/views/#{self.params[:controller]}/#{self.params[:action]}.html.erb").read
+      view_html = ERB.new(vista).result(binding)
+    rescue Errno::ENOENT
+      vista = File.new("./404.html").read
+      view_html = vista
+    end
+    
+    return view_html
   end
   
   def method_missing(sym, *args, &block)
--- prueba/prueba.rb
+++
@@ -1,5 +0,0 @@
-class Prueba
-  def hello(arg)
-    "hello" + arg
-  end
-end
